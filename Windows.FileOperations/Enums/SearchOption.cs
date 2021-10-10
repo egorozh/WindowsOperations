306d801ef38e85fc66d37d9993f14d721414aea2
Egor Zheludkov <zheludkov.egor@mail.ru>
2021-10-10 20:01:20 +0300

    Добавьте файлы проекта.

---
+++ Windows.FileOperations/Enums/SearchOption.cs
@@ -0,0 +1,10 @@
+﻿namespace Windows.FileOperations;
+
+///<summary>
+/// Specify whether to perform the search for files/directories recursively or not.
+/// </summary>
+public enum SearchOption
+{
+    SearchTopLevelOnly = 2,
+    SearchAllSubDirectories = 3,
+}
