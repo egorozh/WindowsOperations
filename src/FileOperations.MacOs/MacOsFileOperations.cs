f440784f30f505c2c8d0932907202bd0c12ffb6c
zheludkov <zheludkov@ddplanet.ru>
2023-08-26 20:10:38 +0300

    feat: added proj for macOs

---
+++ src/FileOperations.MacOs/MacOsFileOperations.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using FileOperations.Abstractions;
+using FileOperations.Abstractions.Enums;
+
+
+namespace FileOperations.MacOs;
+
+
+public class MacOsFileOperations : IFileOperations
+{
+    public void DeleteDirectory(string directory, DeleteDirectoryOption onDirectoryNotEmpty, RecycleOption recycle = RecycleOption.DeletePermanently)
+    {
+        if (recycle == RecycleOption.SendToRecycleBin)
+        {
+            MoveToRecycleBin(directory);
+            return;
+        }
+        
+        Directory.Delete(directory, recursive: onDirectoryNotEmpty == DeleteDirectoryOption.DeleteAllContents);
+    }
+
+    
+    public void DeleteFile(string file, RecycleOption recycle)
+    {
+        if (recycle == RecycleOption.SendToRecycleBin)
+        {
+            MoveToRecycleBin(file);
+            return;
+        }
+        
+        File.Delete(file);
+    }
+    
+
+    private void MoveToRecycleBin(string path)
+    {
+        
+    }
+}
