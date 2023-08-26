67009879b533a59b7a8ed14cf9cea5e47ea5e8df
Егор Желудков <zheludkov@cmsplanet.ru>
2023-08-26 21:20:35 +0300

    feat: implemented move to recBin for macOs

--- src/FileOperations.MacOs/MacOsFileOperations.cs
+++ src/FileOperations.MacOs/MacOsFileOperations.cs
@@ -1,39 +1,53 @@
 ﻿using System.IO;
+using AppKit;
 using FileOperations.Abstractions;
 using FileOperations.Abstractions.Enums;
+using Foundation;
 
 
 namespace FileOperations.MacOs;
 
 
+/// <summary>
+/// Use <see cref="Initialize"/> for working api
+/// </summary>
 public class MacOsFileOperations : IFileOperations
 {
-    public void DeleteDirectory(string directory, DeleteDirectoryOption onDirectoryNotEmpty, RecycleOption recycle = RecycleOption.DeletePermanently)
+    public static void Initialize() => NSApplication.Init();
+
+
+    public void DeleteDirectory(string directory, DeleteDirectoryOption onDirectoryNotEmpty,
+        RecycleOption recycle = RecycleOption.DeletePermanently)
     {
         if (recycle == RecycleOption.SendToRecycleBin)
         {
-            MoveToRecycleBin(directory);
+            MoveToRecycleBin(directory, isDir: true);
             return;
         }
-        
+
         Directory.Delete(directory, recursive: onDirectoryNotEmpty == DeleteDirectoryOption.DeleteAllContents);
     }
 
-    
+
     public void DeleteFile(string file, RecycleOption recycle)
     {
         if (recycle == RecycleOption.SendToRecycleBin)
         {
-            MoveToRecycleBin(file);
+            MoveToRecycleBin(file, isDir: false);
             return;
         }
-        
+
         File.Delete(file);
     }
-    
 
-    private void MoveToRecycleBin(string path)
+
+    private void MoveToRecycleBin(string path, bool isDir)
     {
-        
+        NSFileManager fileManager = NSFileManager.DefaultManager;
+
+        var url = new NSUrl(path, isDir: isDir);
+
+        if (url is not null)
+            fileManager.TrashItem(url, out var res, out var error);
     }
 }
